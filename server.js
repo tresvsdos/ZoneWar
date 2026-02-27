5f4bd7afaec7345520c76d19830b694a3987a384
inigo2002md <inigo2002md@gmail.com>
2026-02-27 00:24:04 +0000

    Fix app hosting deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
